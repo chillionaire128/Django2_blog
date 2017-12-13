f69f59a32ae4512193cd8db3efddfb96d2d23eb6
unknown <ancientcin@gmail.com>
2017-12-12 19:07:33 -0800

    Added basic class views

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.db import models
+from blog.models import Post, Comment
 
 class PostForm(forms.ModelForm):
 
@@ -12,13 +13,13 @@ class PostForm(forms.ModelForm):
             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'}),
         }
 
-class CommentForm(forms.ModelForm)
+class CommentForm(forms.ModelForm):
 
     class Meta():
         model = Comment
         fields = ('author', 'text')
 
         widgets = {
-            'author':forms.TextInput(attrs={'class':'textinputclass'}
+            'author':forms.TextInput(attrs={'class':'textinputclass'}),
             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
         }
